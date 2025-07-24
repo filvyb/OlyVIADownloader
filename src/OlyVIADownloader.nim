570c131c2f21a5782f90a4bd01a29d122758a1fc
Filip Vybihal <101115161+filvyb@users.noreply.github.com>
2025-07-24 17:41:29 +0200

    Add help

--- src/OlyVIADownloader.nim
+++ src/OlyVIADownloader.nim
@@ -26,72 +26,82 @@ when isMainModule:
     flag("-l", "--list", help="List available files instead of downloading them")
     flag("-v", "--version", help="Show version information")
 
-  let args = p.parse(commandLineParams())
+  var downloaderConfig: DownloaderConfig
+  try:
+    var args = p.parse(commandLineParams())
 
-  # Handle version flag
-  if args.version:
-    echo "OlyVIADownloader version ", AppVersion
-    quit(0)
+    # Handle version flag
+    if args.version:
+      echo "OlyVIADownloader version ", AppVersion
+      quit(0)
 
-  var address = if args.address != "":
-    args.address
-  elif getEnv("ADDRESS") != "":
-    getEnv("ADDRESS")
-  else:
-    raise newException(ValueError, "Address is required")
+    var address = if args.address != "":
+      args.address
+    elif getEnv("ADDRESS") != "":
+      getEnv("ADDRESS")
+    else:
+      raise newException(ValueError, "Address is required")
 
-  var database = if args.database != "":
-    args.database
-  elif getEnv("DATABASE") != "":
-    getEnv("DATABASE")
-  else:
-    raise newException(ValueError, "Database is required")
+    var database = if args.database != "":
+      args.database
+    elif getEnv("DATABASE") != "":
+      getEnv("DATABASE")
+    else:
+      raise newException(ValueError, "Database is required")
 
-  var username = if args.username != "":
-    args.username
-  elif getEnv("USERNAME") != "":
-    getEnv("USERNAME")
-  else:
-    raise newException(ValueError, "Username is required")
+    var username = if args.username != "":
+      args.username
+    elif getEnv("USERNAME") != "":
+      getEnv("USERNAME")
+    else:
+      raise newException(ValueError, "Username is required")
 
-  var password = if args.password != "":
-    args.password
-  elif getEnv("PASSWORD") != "":
-    getEnv("PASSWORD")
-  else:
-    raise newException(ValueError, "Password is required")
+    var password = if args.password != "":
+      args.password
+    elif getEnv("PASSWORD") != "":
+      getEnv("PASSWORD")
+    else:
+      raise newException(ValueError, "Password is required")
 
-  var directory = if args.directory != "":
-    args.directory
-  elif getEnv("DIRECTORY") != "":
-    getEnv("DIRECTORY")
-  else:
-    "download"
-  
-  var filter = if args.filter != "":
-    args.filter
-  elif getEnv("FILTER") != "":
-    getEnv("FILTER")
-  else:
-    ""
-  
-  var listFiles = args.list
-  if getEnv("LIST") != "":
-    listFiles = getEnv("LIST").parseBool
+    var directory = if args.directory != "":
+      args.directory
+    elif getEnv("DIRECTORY") != "":
+      getEnv("DIRECTORY")
+    else:
+      "download"
+    
+    var filter = if args.filter != "":
+      args.filter
+    elif getEnv("FILTER") != "":
+      getEnv("FILTER")
+    else:
+      ""
+    
+    var listFiles = args.list
+    if getEnv("LIST") != "":
+      listFiles = getEnv("LIST").parseBool
 
-  var addresssplit = address.split(":")
-  if addresssplit.len != 2:
-    raise newException(ValueError, "Address must be in the format address:port")
+    var addresssplit = address.split(":")
+    if addresssplit.len != 2:
+      raise newException(ValueError, "Address must be in the format address:port")
 
-  let downloaderConfig = DownloaderConfig(
-    address: addresssplit[0],
-    port: addresssplit[1].parseInt,
-    username: username,
-    password: password,
-    database: database,
-    directory: directory,
-    filter: filter,
-    list: listFiles
-  )
+    downloaderConfig = DownloaderConfig(
+      address: addresssplit[0],
+      port: addresssplit[1].parseInt,
+      username: username,
+      password: password,
+      database: database,
+      directory: directory,
+      filter: filter,
+      list: listFiles
+    )
+
+  except ShortCircuit as err:
+    if err.flag == "argparse_help":
+      echo err.help
+      quit(1)
+  except UsageError:
+    stderr.writeLine getCurrentExceptionMsg()
+    quit(1)
 
   waitFor downloader(downloaderConfig)
